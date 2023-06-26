9c3a106b22608141de49c468213671b93fb2e6e8
Sultanbek <sultan.sekenov@yandex.ru>
2023-06-26 18:31:24 +0600

    swiper update

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,30 +1,45 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Productt } from "../../types/product";
 import styles from "./Slider.module.scss";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/swiper-bundle.css";
 type Props = {
   productss: Productt[];
 };
-
+SwiperCore.use([Navigation, Pagination, Autoplay]);
 const Slider: React.FC<Props> = ({ productss }) => {
   return (
-    <div className={styles.slider}>
-      <Carousel infiniteLoop useKeyboardArrows autoPlay>
-        {productss.map((product) => (
-          <div className={styles.slide} key={product.id}>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={2}
+      autoplay={{ delay: 2500, disableOnInteraction: false }}
+      style={{ marginTop: "80px" }}
+    >
+      {productss.map((product) => (
+        <SwiperSlide key={product.id}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
             <img
-              className={styles.image}
               src={product.attributes.image?.data?.attributes.url}
               alt={product.attributes.name}
-              style={{ marginBottom: "100px" }}
+              style={{ width: "400px", height: "auto" }}
             />
-            <p className="legend">{product.attributes.name}</p>
           </div>
-        ))}
-      </Carousel>
-    </div>
+          <div>
+            <p className={styles.name}>{product.attributes.name}</p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
